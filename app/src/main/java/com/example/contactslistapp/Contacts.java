5c38e35212a8848e3ebd6addc9ea2630a2eccfc5
Madhuri-stud <madhuribh2001@gmail.com>
2020-06-23 11:10:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/contactslistapp/Contacts.java
@@ -0,0 +1,65 @@
+package com.example.contactslistapp;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "contacts_table")
+public class Contacts {
+    @PrimaryKey
+    @ColumnInfo(name = "Name")
+    private String mName;
+    @PrimaryKey
+    @ColumnInfo(name = "Phone")
+    private String mPhoneNo;
+    @PrimaryKey
+    @ColumnInfo(name = "Email")
+    private String mEmail;
+    @ColumnInfo(name = "Age")
+    private int mAge;
+    @ColumnInfo(name = "Gender")
+    private int mGender;
+    @ColumnInfo(name = "City")
+    private String mCity;
+    @ColumnInfo(name = "College")
+    private String mCollege;
+
+    public Contacts(@NonNull String name,@NonNull String phoneNo, String email,@NonNull int age,@NonNull int gender,@NonNull String city,@NonNull String college) {
+        this.mName = name;
+        this.mPhoneNo = phoneNo;
+        this.mEmail = email;
+        this.mAge = age;
+        this.mGender = gender;
+        this.mCity = city;
+        this.mCollege = college;
+    }
+
+    public String getName() {
+        return this.mName;
+    }
+
+    public String getPhoneNo() {
+        return this.mPhoneNo;
+    }
+
+    public String getEmail() {
+        return this.mEmail;
+    }
+
+    public int getAge() {
+        return this.mAge;
+    }
+
+    public int getGender() {
+        return this.mGender;
+    }
+
+    public String getCity() {
+        return this.mCity;
+    }
+
+    public String getCollege() {
+        return this.mCollege;
+    }
+}
---
+++ app/src/main/java/com/example/contactslistapp/ContactsDao.java
@@ -0,0 +1,20 @@
+package com.example.contactslistapp;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface ContactsDao {
+
+    @Insert
+    void insert (Contacts contacts);
+
+    @Query("DELETE FROM contacts_table")
+    void deleteAll();
+
+    @Query("SELECT * from contacts_table ORDER BY Name ASC")
+     List<Contacts>  getAllContacts();
+}
